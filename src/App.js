79d0c13a1e2005b6feda84312a73dcd861a75b8d
Bianca91 <biancasteyn91@gmail.com>
2018-03-11 20:34:20 +0100

    Add player progress

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Board from "./containers/Board";
 import CreateGameButton from "./components/CreateGameButton";
+import Progress from "./components/Progress";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1 className="title">0hh1</h1>
+        <div className="actioins">
+          <CreateGameButton />
+        </div>
+
+        <Progress />
+
         <Board />
-        <CreateGameButton />
       </div>
     );
   }
